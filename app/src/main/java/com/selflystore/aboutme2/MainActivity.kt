f5f1d8fe73daf0f7edfe051623ae237a0e31867a
Yiqing Liang <yiqing.liang@hotmail.com>
2024-02-15 15:55:59 +0200

    first codes

---
+++ app/src/main/java/com/selflystore/aboutme2/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.selflystore.aboutme2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.selflystore.aboutme2.ui.theme.Aboutme2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Aboutme2Theme {
+        Greeting("Android")
+    }
+}
